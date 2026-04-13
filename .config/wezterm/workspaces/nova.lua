205d258407a7722f4904682bfefca16593104009
vbenoot-itsme <vincent.benoot@itsme-id.com>
2026-04-13 09:10:27 +0200

    adds notes tab

--- .config/wezterm/workspaces/nova.lua
+++ .config/wezterm/workspaces/nova.lua
@@ -1,4 +1,25 @@
 local M = {}
+local notesDir = "/Users/vincentbenoot/notes"
+
+local function split(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t = {}
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
+
+local function openNotes(cwd)
+	local splitted = split(cwd, "/")
+
+	local dirName = splitted[#splitted]
+	local notesOpen = notesDir .. "/" .. dirName
+
+	return notesOpen
+end
 
 M.create = function(cwd, label)
   local wezterm = require 'wezterm'
@@ -22,15 +43,18 @@ M.create = function(cwd, label)
 	helix_pane:send_text("hx main\n")
 	lazygit_pane:send_text("lazygit -p main\n")
 
-	local _, test_pane = window:spawn_tab {cwd = cwd .. "/main"}
+	local _, clanker_pane = window:spawn_tab {cwd = cwd .. "/clanker"}
 
-	local opencode_pane = test_pane:split({
+	local opencode_pane = clanker_pane:split({
 		direction = "Right",
 		size = 0.5,
 		cwd = cwd .. "/clanker",
 	})
 
 	opencode_pane:send_text("opencode\n")
+
+	local _, notes_pane = window:spawn_tab { cwd = openNotes(cwd)}
+	notes_pane:send_text("hx .\n")
 
 	helix_pane:activate {}
 end
