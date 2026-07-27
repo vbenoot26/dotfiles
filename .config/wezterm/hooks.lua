e7d3a1e8a6c24cb2a6705a689bfdcc493c00c388
vbenoot-itsme <vincent.benoot@itsme-id.com>
2026-07-27 14:25:32 +0200

    make golden ratio a plugin

--- .config/wezterm/hooks.lua
+++ .config/wezterm/hooks.lua
@@ -9,20 +9,20 @@ local function segments_for_right_status(window)
 end
 
 wezterm.on('augment-command-palette', function(_, _)
-  return {
-    {
-      brief = 'Kill workspace',
-      icon = 'md_skull',
+	return {
+		{
+			brief = 'Kill workspace',
+			icon = 'md_skull',
 
-      action = wezterm.action_callback(function(currentwindow, _, _)
+			action = wezterm.action_callback(function(currentwindow, _, _)
 				local killworkspace = require("actions.workspace-kill")
 				local w = currentwindow:active_workspace()
-      	killworkspace.kill_workspace(w)
-    	end
-      )
-      ,
-    },
-  }
+				killworkspace.kill_workspace(w)
+			end
+			)
+			,
+		},
+	}
 end)
 
 wezterm.on("update-status", function(window, _)
@@ -70,52 +70,6 @@ wezterm.on("update-status", function(window, _)
 end)
 
 wezterm.on("update-status", function(window, pane)
-	local tab = window:active_tab()
-
-	local window_dims = window:get_dimensions()
-
-	local pane_info = {}
-	for _, curr_pane in ipairs(tab:panes_with_info()) do
-		if curr_pane.is_active then
-			pane_info = curr_pane
-		end
-	end
-
-	if pane_info.is_zoomed then
-		return
-	end
-
-	local tab_dims = tab:get_size()
-
-	local target_width = math.floor(2 * tab_dims.cols / 3)
-	local target_height = math.floor(2 * tab_dims.rows / 3)
-
-	local pane_dims = pane:get_dimensions()
-	local pane_width = pane_dims.cols
-	local pane_height = pane_dims.viewport_rows
-
-	local delta_width = math.tointeger(target_width - pane_width)
-	local delta_height = math.tointeger(target_height - pane_height)
-
-	local act = wezterm.action
-	if pane_width < target_width then
-		local dir = 'Right'
-		if pane_info.left > 0 then
-			dir = 'Left'
-		end
-		wezterm.log_info("adjusting: " .. delta_width)
-		wezterm.log_info("dir: " .. dir)
-		window:perform_action(act.AdjustPaneSize{dir, delta_width}, pane)
-	end
-
-
-	if pane_height < target_height then
-	local dir = 'Down'
-		if pane_info.top > 0 then
-			dir = 'Up'
-		end
-		wezterm.log_info("adjusting: " .. delta_height)
-		wezterm.log_info("dir: " .. dir)
-		window:perform_action(act.AdjustPaneSize{dir, delta_height}, pane)
-	end
+	local gr = wezterm.plugin.require 'file:///Users/vincentbenoot/projects/personal/golden-ratio'
+	gr.apply_golden_ratio(window, pane)
 end)
